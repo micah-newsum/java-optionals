5c9766622ee338b52390279b2e65c27929b12f4d
Micah Newsum <mnewsum@glenair.com>
2021-02-16 08:15:59 -0800

    Add Optional model.

---
+++ src/main/java/com/newsum/model/OptionalComputer.java
@@ -0,0 +1,10 @@
+package com.newsum.model;
+
+import java.util.Optional;
+
+public class OptionalComputer {
+  private Optional<OptionalSoundcard> soundcard;
+  public Optional<OptionalSoundcard> getSoundcard(){
+    return soundcard;
+  }
+}
---
+++ src/main/java/com/newsum/model/OptionalSoundcard.java
@@ -0,0 +1,10 @@
+package com.newsum.model;
+
+import java.util.Optional;
+
+public class OptionalSoundcard {
+  private Optional<USB> usb;
+  public Optional<USB> getUSB() {
+    return usb;
+  }
+}
